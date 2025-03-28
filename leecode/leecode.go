c773b36a00dfa6c4cab24609862eb9b6f92a2045
ying.li <18616649902@126.com>
2025-03-28 10:21:21 +0800

    [feat] update

--- leecode/leecode.go
+++ leecode/leecode.go
@@ -2,19 +2,19 @@ package leecode
 
 // 无重复字符的最长子串
 func lengthOfLongestSubstring(s string) int {
-	left, right, maxlength := 0, 0, 0
-	set := make(map[byte]bool)
+	set := map[byte]bool{}
+	right, left, maxLen := 0, 0, 0
 	for right < len(s) {
 		if !set[s[right]] {
 			set[s[right]] = true
 			right++
-			if len(set) > maxlength {
-				maxlength = len(set)
+			if len(set) > maxLen {
+				maxLen = len(set)
 			}
 		} else {
 			delete(set, s[left])
 			left++
 		}
 	}
-	return maxlength
+	return maxLen
 }
